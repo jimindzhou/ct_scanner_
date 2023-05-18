24909a1b00dd26f0834b26e82e43f19f703b552c
Jose Ubillus <jose.ubillus@beg.utexas.edu>
2023-05-18 16:20:18 -0500

    Last update with coarsening, and cropping/centering

--- crop.py
+++ crop.py
@@ -5,8 +5,8 @@ import numpy as np
 import imutils
 
 # read dry and wet image
-dry = dc.dcmread('C:/Users/ubillusj/Desktop/Almostafa/N2_dry/1069.dcm')
-wet = dc.dcmread('C:/Users/ubillusj/Desktop/Almostafa/100%_brine/1069.dcm')
+dry = dc.dcmread('C:/Users/ubillusj/Desktop/Almostafa/N2_dry/Raw/0004.dcm')
+wet = dc.dcmread('C:/Users/ubillusj/Desktop/Almostafa/100%_brine/Raw/0004.dcm')
 
 # get pixel array
 dry_array = dry.pixel_array
@@ -20,8 +20,20 @@ img = wet_array.astype(float)
 img_2d = (np.maximum(img,0) / img.max()) * 255.0
 img_2d_wet = np.uint8(img_2d)
 
+def transform_to_hu(raw,array):
 
-def align_images(dry_array, wet_array, maxFeatures=500, keepPercent=0.5,debug=False):
+    intercept = raw.RescaleIntercept
+
+    slope = raw.RescaleSlope
+
+    hu_image = array * slope + intercept
+
+    return hu_image
+    
+def align_images(dry_array, wet_array, maxFeatures=5000, keepPercent=0.5,debug=False):
+
+    dry_array = cv2.bilateralFilter(dry_array,9,75,75)
+    wet_array = cv2.bilateralFilter(wet_array,9,75,75)
 
     ## ORB detector (keypoints and descriptors)
     orb = cv2.ORB_create(maxFeatures)
@@ -29,8 +41,8 @@ def align_images(dry_array, wet_array, maxFeatures=500, keepPercent=0.5,debug=Fa
     (kpsB, descsB) = orb.detectAndCompute(wet_array, None)
 
     ## Match features
-    method = cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
-    matcher = cv2.DescriptorMatcher_create(method)
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    #matcher = cv2.DescriptorMatcher_create(method,crosscheck=True)
     matches = matcher.match(descsA, descsB, None)
 
     ## Sort matches by score
@@ -69,19 +81,61 @@ def align_images(dry_array, wet_array, maxFeatures=500, keepPercent=0.5,debug=Fa
     ## Return the aligned image
     return aligned
 
-aligned = align_images(img_2d_dry, img_2d_wet, debug=False)
+aligned = align_images(img_2d_dry, img_2d_wet, debug=True)
+
+def center_image(image):
+  height, width = image.shape
+  print(img.shape)
+  wi=(width/2)
+  he=(height/2)
+  print(wi,he)
+
+  ret,thresh = cv2.threshold(image,95,255,0)
+
+  M = cv2.moments(thresh)
+
+  cX = int(M["m10"] / M["m00"])
+  cY = int(M["m01"] / M["m00"])
+
+  offsetX = (wi-cX)
+  offsetY = (he-cY)
+  T = np.float32([[1, 0, offsetX], [0, 1, offsetY]]) 
+  centered_image = cv2.warpAffine(image, T, (width, height))
+
+  return centered_image
+
+#aligned = center_image(aligned)
 
 def detect_circles(image):
+    fx,fy,fr = 0,0,0
     output = image.copy()
     circles = cv2.HoughCircles(image, method = cv2.HOUGH_GRADIENT, dp = 5, minDist = 500,maxRadius=70,minRadius=65)
 
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
-        for (x,y,r) in circles:
-            cv2.circle(output, (x,y), r, (0,255,0), 4)
-            cv2.rectangle(output, (x-5, y-5), (x+5, y+5), (0,128,255), -1)
+        fx, fy , fr = circles[0]
+    
+    return fx, fy, fr
 
-    return plt.imshow(output, cmap=plt.cm.bone)
+def crop_image(image):
+    x, y ,r = detect_circles(image)
+    crop_img = image[y-r:y+r, x-r:x+r]
 
-plt.imshow(aligned, cmap=plt.cm.bone)
-detect_circles(aligned)
+    # create a mask
+    mask = np.full((crop_img.shape[0], crop_img.shape[1]), 0, dtype=np.uint8) 
+    # create circle mask, center, radius, fill color, size of the border
+    cv2.circle(mask,(r,r), r, (255,255,255),-1)
+    # get only the inside pixels
+    fg = cv2.bitwise_or(crop_img, crop_img, mask=mask)
+    
+    mask = cv2.bitwise_not(mask)
+    background = np.full(crop_img.shape, 255, dtype=np.uint8)
+    bk = cv2.bitwise_or(background, background, mask=mask)
+    final = cv2.bitwise_or(fg, bk)
+
+    return plt.imshow(final, cmap=plt.cm.bone)
+
+
+#plt.imshow(aligned, cmap=plt.cm.bone)
+
+#crop_image(aligned)
--- functions.py
+++ functions.py
@@ -2,11 +2,11 @@ import pydicom as dicom
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+from scipy import stats
+import scipy.ndimage
 import imutils
 import glob
-
-dry_path = 'C:/Users/ubillusj/Desktop/Almostafa/N2_dry/'
-wet_path = 'C:/Users/ubillusj/Desktop/Almostafa/100%_brine/'
+from PIL import Image
 
 def read_dicom(path):
 
@@ -20,13 +20,13 @@ def convert_grayscale(slices):
 
     for i in range(len(slices)):
         img = slices[i].astype(float)
-        img_2d = (np.maximum(img,0) / img.max()) * 255.0
+        img_2d = (np.maximum(img,0) / img.max()) * 255
         img_2d = np.uint8(img_2d)
         slices_gray.append(img_2d)
 
     return slices_gray
 
-def align_images(dry_slices,wet_slices,maxFeatures=500,keepPercent= 0.5, debug=False):
+def align_images(dry_slices,wet_slices,maxFeatures=500,keepPercent= 0.2, debug=False):
     
     wet_aligned = []
 
@@ -53,7 +53,7 @@ def align_images(dry_slices,wet_slices,maxFeatures=500,keepPercent= 0.5, debug=F
         matches = matches[:keep]
 
         # Visualize matches
-        if (debug == True and i == 10):
+        if (debug == True and s == 1000):
             matchedVis = cv2.drawMatches(dry_slices[s], kpsA, wet_slices[s], kpsB, matches, None)
             matchedVis = imutils.resize(matchedVis, width=1000)
             cv2.imshow("Matched Keypoints", matchedVis)
@@ -86,7 +86,126 @@ def align_images(dry_slices,wet_slices,maxFeatures=500,keepPercent= 0.5, debug=F
 
     return wet_aligned
 
+def center_image(slices):
 
+    centered_slices = []
+
+    for s in range(len(slices)):
+        
+        height, width = slices[s].shape
+        wi, he = width/2, height/2
+        ret,thresh = cv2.threshold(slices[s], 110, 255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        M = cv2.moments(thresh)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+
+        dx = wi-cx
+        dy = he-cy
+
+        T = np.float32([[1,0,dx],[0,1,dy]])
+        dst = cv2.warpAffine(slices[s],T,(width,height))
+
+        centered_slices.append(dst)
+    
+    return centered_slices
+
+def detect_circles(image):
+    x, y, r = 0, 0, 0
+    output = image.copy()
+    output = cv2.medianBlur(output, 5)
+    output = cv2.threshold(output, 110, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    circles = cv2.HoughCircles(image, method = cv2.HOUGH_GRADIENT, dp = 5, minDist = 500,maxRadius=70,minRadius=65)
+
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("uint8")
+        for (x,y,r) in circles:
+            print(x,y,r)
+            
+    return x,y,r
+
+def circles_list(slices):
+    x, y, r = [], [], []
+    for s in range(len(slices)):
+        output = slices[s].copy()
+        output = cv2.medianBlur(output, 5)
+        output = cv2.threshold(output, 110, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        circles = cv2.HoughCircles(slices[s], method = cv2.HOUGH_GRADIENT, dp = 5, minDist = 200,maxRadius=70,minRadius=65)
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("uint8")
+            for (x1,y1,r1) in circles:
+                x.append(x1)
+                y.append(y1)
+                r.append(r1)
+
+    x_mode = stats.mode(x)
+    y_mode = stats.mode(y)
+    r_mode = stats.mode(r)
+
+    return x_mode[0][0], y_mode[0][0], r_mode[0][0]
+
+def mask_images(slices):
+    masked_slices = []
+    x_mode, y_mode, r_mode = circles_list(slices)
+
+    for s in range(len(slices)):
+        x,y,r = detect_circles(slices[s])
+
+        if x != 252:
+            x = 252
+        if y != 248:
+            y = 248
+
+        # create a mask
+        mask = np.full((slices[s].shape[0], slices[s].shape[1]), 0, dtype=np.uint8) 
+        # create circle mask, center, radius, fill color, size of the border
+        cv2.circle(mask,(x,y), r, (255,255,255),-1)
+        # get only the inside pixels
+        fg = cv2.bitwise_or(slices[s], slices[s], mask=mask)
+        
+        mask = cv2.bitwise_not(mask)
+        background = np.full(slices[s].shape, 255, dtype=np.uint8)
+        bk = cv2.bitwise_or(background, background, mask=mask)
+        final = cv2.bitwise_or(fg, bk)
+        cropped = final[150:350,150:350]
+        masked_slices.append(cropped)
+        
+    return masked_slices
+
+def resample(slices, new_spacing=[1,1,1]):
+    resampled = []
+    spacing = np.array([0.25,0.25,0.25])
+    slices = np.dstack(slices)
+    resize_factor = np.divide(spacing,new_spacing)
+    new_real_shape = np.multiply(slices.shape, resize_factor)
+    real_resize_factor = np.divide(new_real_shape,slices.shape)
+    new_spacing = np.divide(spacing,real_resize_factor)
+
+
+    resampled = scipy.ndimage.interpolation.zoom(slices, real_resize_factor)
+        
+    return resampled
+
+def z_profiling(slices):
+    z_profile = []
+    number = []
+    for s in range(len(slices)):
+        z_profile.append(np.mean(slices[s]))
+        number.append(s)
+    
+    plt.plot(number,z_profile)
+    plt.xlabel('Slice Number')
+    plt.ylabel('Mean CT')
+    plt.title('Z-Profile')
+    plt.ylim(0,255)
+    plt.show()
+
+    return z_profile
+def save_tif(slices,path):
+    for s in range(len(slices)):
+        im = Image.fromarray(slices[s])
+        im.save(path + str(s) + '.tif')
+
+    return print('Done')
 
 def compare_images(dry_slices,wet_slices,wet_aligned,i=100):
 
@@ -103,14 +222,3 @@ def compare_images(dry_slices,wet_slices,wet_aligned,i=100):
     
     return plt.show()
 
-
-
-dry_slices = read_dicom(dry_path)
-wet_slices = read_dicom(wet_path)
-
-dry_slices = convert_grayscale(dry_slices)
-wet_slices = convert_grayscale(wet_slices)
-
-wet_aligned = align_images(dry_slices,wet_slices,debug=False)
-
-compare_images(dry_slices,wet_slices,wet_aligned,i=10)
