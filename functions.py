48f889af32938b2d65a60119c82e482fae1fd510
Jose Ubillus <jose.ubillus@beg.utexas.edu>
2023-08-03 20:02:46 -0500

    Updated version

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pydicom as dicom
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 import numpy as np
 from scipy import stats 
 import scipy.ndimage
@@ -27,7 +28,7 @@ def read_tif(folder_path,total_scans):
 
     return slices
 
-def mask_images(slices,cx,cy,radius,last_slice):
+def mask_images(slices,cx,cy,radius,first_slice,last_slice):
     slices_output = slices.copy()
     height, width = slices.shape[0], slices.shape[1]
     # create circular mask
@@ -41,7 +42,7 @@ def mask_images(slices,cx,cy,radius,last_slice):
     for i in range(slices.shape[2]):
         slices_output[:,:,i] = slices[:,:,i]*circle_array
 
-    final = slices_output[int(cy-radius):int(cy+radius),int(cx-radius):int(cx+radius),0:last_slice]
+    final = slices_output[int(cy-radius):int(cy+radius),int(cx-radius):int(cx+radius),first_slice:last_slice]
 
     return final
 
@@ -61,25 +62,26 @@ def resample(slices,size = 3, new_spacing=[1,1,0.25]):
     real_resize_factor = np.divide(new_real_shape,slices.shape)
     new_spacing = np.divide(spacing,real_resize_factor)
 
-    resampled = scipy.ndimage.interpolation.zoom(slices, real_resize_factor,order=1)
+    resampled = scipy.ndimage.interpolation.zoom(slices, real_resize_factor,order=0)
 
     return resampled
 
-def z_profiling(slices):
-    z_profile = []
-    number = []
-    for s in range(slices.shape[2]):
-        slices[:,:,s] = slices[:,:,s][slices[:,:,s] != 0]
-        z_profile.append(np.mean(slices[:,:,s]))
-        number.append(s)
-    
-    plt.plot(number,z_profile)
+def z_profiling(sdict):
+    z_profiles= {}
+    for key in sdict.keys():
+        number = np.arange(0,sdict[key].shape[2],1)
+        sdict[key] = sdict[key][sdict[key] != 0]
+        z_profiles[key] = np.mean(sdict[key])
+        
+        plt.plot(number,z_profiles[key],label=key)
+
+    plt.legend()
     plt.xlabel('Slice Number')
     plt.ylabel('Mean CT')
     plt.title('Z-Profile')
     plt.show()
 
-    return z_profile
+    return z_profiles
 
 def histograms(slices):
     slices = slices[slices != 0 ]
@@ -87,7 +89,7 @@ def histograms(slices):
 
     # Plot the histogram.
     plt.hist(slices.flatten(),density=True,bins=100, color='c')
-    xmin, xmax = plt.xlim()
+    xmin, xmax = mu - 3*std, mu + 3*std
     x = np.linspace(xmin, xmax, 10000)
     p = stats.norm.pdf(x, mu, std)
     plt.plot(x, p, 'k', linewidth=2)
@@ -191,6 +193,150 @@ def volume_registration(images_fixed,images_moving):
     registered_array = np.array(np.transpose(sitk.GetArrayFromImage(moving_resampled), (1, 2, 0)))
 
     return registered_array
+
+# Plot multiple slices of the different scans
+def plot_slices(slices, nx, ny):
+    dims = slices.shape
+    s = np.linspace(0,dims[2]-1,nx*ny).astype(int)
+    fig, axs = plt.subplots(ny, nx, figsize=(15, 15))
+    for i in range(ny):
+        for j in range(nx):
+            im = axs[i, j].imshow(slices[:, :, s[i * nx + j]], cmap="gray")
+            axs[i,j].set_title('Slice ' + str(s[i * nx + j]))
+            axs[i, j].axis("off")
+
+    fig.colorbar(im, ax=axs.ravel().tolist(), orientation = 'vertical',shrink=0.5,pad=0.04)
+
+    return plt.show()
+
+# Plot multiple histograms in one figure of different scans
+def plot_multiple_histograms(s1,s2,label1,label2):
+    s1 = s1[s1 != 0 ]
+    s2 = s2[s2 != 0 ]
+    mu1, std1 = stats.norm.fit(s1.flatten())
+    mu2, std2 = stats.norm.fit(s2.flatten())
+
+    fig, ax = plt.subplots(figsize=(5,5))
+    ax.hist(s1.flatten(),density=True,bins=100, color='c',label=label1 + ' (mean = ' + str(round(mu1,2)) + ', std = ' + str(round(std1,2)) + ')')
+    ax.hist(s2.flatten(),density=True,bins=100, color='m',label=label2 + ' (mean = ' + str(round(mu2,2)) + ', std = ' + str(round(std2,2)) + ')')
+
+    xmin1 = mu1-3*std1
+    xmax1 = mu1+3*std1
+    xmin2 = mu2-3*std2
+    xmax2 = mu2+3*std2
+
+    x1 = np.linspace(xmin1, xmax1, 10000)
+    x2 = np.linspace(xmin2, xmax2, 10000)
+    p1 = stats.norm.pdf(x1, mu1, std1)
+    p2 = stats.norm.pdf(x2, mu2, std2)
+    ax.plot(x1, p1, 'k', linewidth=1,linestyle='--')
+    ax.plot(x2, p2, 'k', linewidth=1,linestyle='--')
+
+    ax.set_xlabel('CT')
+    ax.set_ylabel('Frequency')
+    ax.set_xlim(0,2000)
+    plt.legend(loc='upper right')
+
+    return plt.show()
+
+# Porosity and saturation estimations
+def porosity(dry, wet):
+    wet_temp = wet.copy()
+    dry_temp = dry.copy()
+    wet_temp[wet_temp == 0 ] = np.nan
+    dry_temp[dry_temp == 0 ] = np.nan
+
+    ct_dry_avg = np.nanmean(dry_temp)
+    ct_wet_avg = np.nanmean(wet_temp)
+
+    # Core level porosity
+    por_core = (ct_wet_avg - ct_dry_avg) / (1000-0)
+
+    # Slice level porosity
+    slice_dry_avg = np.nanmean(dry_temp, axis=(0,1))
+    slice_wet_avg = np.nanmean(wet_temp, axis=(0,1))
+
+    por_slice = (slice_wet_avg - slice_dry_avg) / (1000-0)
+
+    # Voxel-level porosity
+    por_voxel = (wet - dry) / (1000-0)
+
+    # plot both slice level and multiple-voxel level porosity
+    fig, axs = plt.subplots(3,4, figsize=(15, 10),dpi=300)
+    gs = axs[0,0].get_gridspec()
+    for ax in axs[0:,0]:
+        ax.remove()
+
+    axbig = fig.add_subplot(gs[0:,0])
+    axbig.plot(por_slice,range(len(por_slice)),color='k',linestyle="--",linewidth=2)
+    axbig.set_xlabel('Porosity')
+    axbig.set_ylabel('Slice')
+    axbig.set_xlim(0,0.5)
+    axbig.set_title('Slice-level porosity')
+
+    s = np.linspace(0,por_voxel.shape[2]-1,12).astype(int)
+    for i in range(3):
+        for j in range(3):
+            im = axs[i,j+1].imshow(por_voxel[:, :, s[i * 3 + j]], cmap="gray",vmin=0,vmax=0.5)
+            axs[i,j+1].set_title('Slice ' + str(s[i * 3 + j]))
+            axs[i, j+1].axis("off")
+            
+    fig.colorbar(im, ax=axs[0:,1:], orientation = 'vertical',shrink=0.5,ticks=[0,0.2,0.4,0.6,0.8,1])
+    fig.text(0.35,0.06,'Core level porosity estimate: ' + str(round(por_core,3)),
+             fontsize=14, bbox={'facecolor': 'lightblue', 'alpha': 0.5, 'pad': 5})
+   
+    return plt.show()
+
+def saturation(wet,co2,twophase):
+    wet_temp = wet.copy()
+    co2_temp = co2.copy()
+    tph_temp = twophase.copy()
+    wet_temp[wet_temp == 0 ] = np.nan
+    co2_temp[co2_temp == 0 ] = np.nan
+    tph_temp[tph_temp == 0 ] = np.nan
+
+    # Core level saturation 
+    ctexpr = np.nanmean(tph_temp)
+    ctco2 = np.nanmean(co2_temp)
+    ctwater = np.nanmean(wet_temp)
+
+    sat_core = (ctwater - ctexpr) / (ctwater - ctco2)
+
+    # Slice level saturation
+    slice_expr_avg = np.nanmean(tph_temp, axis=(0,1))
+    slice_co2_avg = np.nanmean(co2_temp, axis=(0,1))
+    slice_water_avg = np.nanmean(wet_temp, axis=(0,1))
+
+    sat_slice = (slice_water_avg - slice_expr_avg) / (slice_water_avg - slice_co2_avg)
+
+    # Voxel-level saturation
+    sat_voxel = (wet - twophase) / (wet - co2)
+
+    # plot both slice level and multiple-voxel level saturation
+    fig, axs = plt.subplots(3,4, figsize=(15, 10),dpi=300)
+    gs = axs[0,0].get_gridspec()
+    for ax in axs[0:,0]:
+        ax.remove()
+
+    axbig = fig.add_subplot(gs[0:,0])
+    axbig.plot(sat_slice,range(len(sat_slice)),color='k',linestyle="--",linewidth=2)
+    axbig.set_xlabel('Saturation')
+    axbig.set_ylabel('Slice')
+    axbig.set_xlim(0,1)
+    axbig.set_title('Slice-level saturation')
+
+    s = np.linspace(0,sat_voxel.shape[2]-1,12).astype(int)
+    for i in range(3):
+        for j in range(3):
+            im = axs[i, j+1].imshow(sat_voxel[:, :, s[i * 3 + j]], cmap="jet",vmin=0,vmax=1)
+            axs[i,j+1].set_title('Slice ' + str(s[i * 3 + j]))
+            axs[i, j+1].axis("off")
+    
+    fig.colorbar(im, ax=axs[0:,1:], orientation = 'vertical',shrink=0.5,pad=0.04,ticks=[0,0.2,0.4,0.6,0.8,1])
+    fig.text(0.35,0.06,'Core level saturation estimate: ' + str(round(sat_core,3)),
+             fontsize=14, bbox={'facecolor': 'lightblue', 'alpha': 0.5, 'pad': 5})
+
+    return plt.show()
 
 
 
